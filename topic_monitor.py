0e08d97c99dc523e4529dd79859eaed55d7a3472
Chris Choi <chutsu@gmail.com>
2019-12-12 16:32:09 +0000

    Add bag2csv.py and show_depth.py

--- topic_monitor.py
+++ topic_monitor.py
@@ -8,26 +8,19 @@ ODOM_TOPIC = "/ucl_0/vrpn_client/estimated_odometry"
 VICON_TOPIC = "/vrpn_client_node/ucl_0/pose"
 
 timeout_threshold = 1.0  # in seconds
-odom_ok = True
 odom_last_update = None
-vicon_ok = True
-odom_last_update = None
+vicon_last_update = None
 
 
 def odom_cb(msg):
     if odom_last_update is None:
         odom_last_update = time()
 
-    if (time() - odom_last_update) > timeout_threshold:
-        odom_ok = False
-
 
 def vicon_cb(msg):
     if vicon_last_update is None:
         vicon_last_update = time()
 
-    if (time() - vicon_last_update) > timeout_threshold:
-        vicon_ok = False
 
 
 if __name__ == "__main__":
@@ -37,14 +30,16 @@ if __name__ == "__main__":
     rospy.Subscriber(VICON_TOPIC, PoseWithCovarianceStamped, vicon_cb)
 
     while True:
-        if odom_ok:
-            rospy.loginfo("Odom is ok!")
-        else:
+        # Check odom
+        if (time() - odom_last_update) > timeout_threshold:
             rospy.logerr("Odom is NOT PUBLISHING!")
-
-        if vicon_ok:
-            rospy.loginfo("VICON is ok!")
         else:
+            rospy.loginfo("Odom is ok!")
+
+        # Check vicon
+        if (time() - vicon_last_update) > timeout_threshold:
             rospy.logerr("VICON is NOT PUBLISHING!")
+        else:
+            rospy.loginfo("VICON is ok!")
 
         node_rate.sleep()
